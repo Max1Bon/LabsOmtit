da19e2e392101b3ac339f1b2e7541d11ec7a4ac2
Max1Bon <sir.mihailenko@yandex.ru>
2016-12-09 17:22:35 +0600

    Add project files.

---
+++ ReadFiles/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace ReadFiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int a = 0;
+            int t = int.Parse(Console.ReadLine());
+            //проверка на наличе файла
+            if (!File.Exists("test" + t + ".txt"))
+            {
+                Console.WriteLine("error");
+                return;
+            }
+
+            StreamReader sr2 = new StreamReader("test" + t + ".txt");
+
+            //вывод содержимого
+            while (!sr2.EndOfStream) //пока не закончился файл
+            {
+                String str = sr2.ReadLine();
+                //выводим год
+                //Console.WriteLine(str.Substring(0, 4));
+
+                //выводим день
+                string day;
+                if (str.IndexOf(',', 5) == 6)
+                {
+                    //Console.WriteLine(str.Substring(5, 1));
+                    day = str.Substring(5, 1);
+
+                }
+                else
+                {
+                    //Console.WriteLine(str.Substring(5, 2));
+                    day = str.Substring(5, 2);
+                }
+
+                //Выводим месяц
+                int begin = str.IndexOf(',', 5)+1;
+                int count = str.Length - begin;
+               // Console.WriteLine(str.Substring(begin, count));
+                
+                string month = str.Substring(begin, count);
+                 
+                string year = str.Substring(0, 4);
+
+                //если 
+                if (month == "1")
+                {
+                    month = "Января";
+                }
+                else if (month == "2")
+                {
+                    month = "Декабря";
+                }
+                else
+                {
+                    month = "месяц неверный";
+                }
+
+                Console.WriteLine("day: " + day + "month: "+ month + "year: "+ year);
+                a++;
+
+               
+            }
+
+            //закрытие файоа
+            sr2.Close();
+        }
+    }
+}
---
+++ task2/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("введите номер файла");
+            int file_number = int.Parse(Console.ReadLine());
+
+            //проверка на наличе файла
+            if (!File.Exists("test" + file_number + ".txt"))
+            {
+                Console.WriteLine("error");
+                return;
+            }
+
+            StreamReader sr1 = new StreamReader("test" + file_number + ".txt");
+            
+            string[] text = File.ReadAllLines("test" + Convert.ToString(file_number) + ".txt");
+            double sum = 0;
+
+            //считаем среднее арифмитическое
+            
+            for (int i = 1; i < text.Length; i++)
+            {
+                if (Convert.ToDouble(text[i])<=20)
+                {
+                    sum = sum + Convert.ToDouble(text[i]);
+                }
+            }
+
+            //вывод значения sum
+            sum = sum / text.Length - 1;
+            Console.WriteLine(sum);
+
+            //Вывод файла
+            for (int i = 0; i < text.Length; i++)
+            {
+                Console.WriteLine(text[i]);
+            }
+            //вывод содержимого
+            /*while (!sr1.EndOfStream) //пока объект sr1 не закончится
+            { 
+              int[] text_array = sr1.ReadLine();
+            }
+            */
+            //Найти среднее арифметическое чисел, значение которых не превышает 20. (Если число в файле > 20, то не использовать его)
+            //Первое число в файле – количество чисел. Оно не участвует в вычислениях. (То есть будет массив? Числа по линиям как бы разбиты)
+            //Данные в файле написаны в столбик без дополнительного оформления.
+        }
+    }
+}
