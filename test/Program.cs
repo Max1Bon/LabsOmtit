dbcbbc37c40fa64fbf0f0e6e91a43fcb809c67a8
Max1Bon <sir.mihailenko@yandex.ru>
2017-01-18 15:02:31 +0600

    is dirty

--- test/Program.cs
+++ test/Program.cs
@@ -6,10 +6,20 @@ using System.Threading.Tasks;
 
 namespace test
 {
+    public class point
+    {
+        public int x;
+        public int y;
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            point p1 = null;
+            point p2 = new point();
+            p2.x = 19;
+            p1.y = 17;
+            Console.WriteLine(p2.x + "," + p2.y);
         }
     }
 }
